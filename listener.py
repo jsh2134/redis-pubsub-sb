b900882c1fa1c0fa2c35273a72dd21e355e78cd8
Jeff Hull <jeff@fitocracy.com>
2015-02-14 10:06:27 -0500

    cleanup

--- listener.py
+++ listener.py
@@ -2,22 +2,26 @@ import redis
 from channels import NEW
 from channels import UPDATES
 
-def listen(channels):
-    r = redis.Redis("localhost")
-    ps = r.pubsub()
-    ps.subscribe(channels)
-    # Print messages
-    for item in ps.listen():
-        alert(item['channel'], item['data'])
+class Listener(object):
 
-def alert(channel, message):
-    # TODO get users subscribed to this channel
-    #   subscribers = get_subscribers(channel)
-    # TODO send webhook/email/push to each of them
-    #   for subscriber in subscribers:
-    #       subscriber.notify()
-    print "[%s] %s" % (channel, message)
+    def __init__(self, channels):
+        self.channels = channels
+        self.client = redis.Redis("localhost")
+        self.ps = self.client.pubsub()
+
+    def listen(self):
+        self.ps.subscribe(self.channels)
+        for item in self.ps.listen():
+            self.alert(item['channel'], item['data'])
+
+    def alert(self, channel, message):
+        # TODO get users subscribed to this channel
+        #   subscribers = get_subscribers(channel)
+        # TODO send webhook/email/push to each of them
+        #   for subscriber in subscribers:
+        #       subscriber.notify()
+        print "[%s] %s" % (channel, message)
 
 if __name__ == "__main__":
     # Listen for channel updates
-    listen([UPDATES, NEW])
+    Listener([UPDATES, NEW]).listen()
